e0abb1dd3e37e7eff1e4d2caaf5f9f1f529df6f0
Ryan Mullin <ryan.mullin12@gmail.com>
2023-08-18 14:37:27 -0700

    Added init.lua for neovim

--- dot_config/i3/scripts/executable_picom_switch.sh
+++ dot_config/i3/scripts/executable_picom_switch.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
 
+# Toggles whether or not the picom compositor is running. Sometimes Picom can
+# mess with certain applications and cause slowdown, so you can bind this
+# to a hotkey in i3 (I use mod + p) to toggle it off temporarily
+if pgrep picom; then
+
---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,46 @@
+-- set tabs to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- enable line numbers
+vim.opt.number = true
+
+-- shell specific configs
+local shell_group = vim.api.nvim_create_augroup('Shell', { clear = true })
+
+-- sets the filetype for bash files that don't have the .sh extension
+vim.api.nvim_create_autocmd(
+	{'BufRead', 'BufNewFile'},
+	{ 
+		pattern={'.bashrc', '.profile', '.bash_profile'}, 
+		group=shell_group,
+		command='set filetype sh'
+	}
+)
+
+-- Automatically adds a shebang to the top of a shell file on creation
+-- Also automatically makes the file executable on buffer write
+vim.api.nvim_create_autocmd(
+	'BufNewFile',
+	{
+		group = shell_group,
+    pattern = "*",
+		callback = function (opts)
+      local ft = vim.filetype.match({ buf = opts.buf })
+			if ft == 'sh' then
+				vim.api.nvim_put({'#!/usr/bin/env sh', ''}, "", false, true)
+        vim.api.nvim_create_autocmd(
+          'BufWritePost',
+          {
+            buffer = opts.buf,
+            callback = function(opts) 
+              vim.fn.system({'chmod', '+x', opts.file})
+            end
+          }
+        )
+			end
+		end
+	}
+)
+
