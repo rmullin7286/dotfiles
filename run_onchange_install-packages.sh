6058711523d013cdf554ac8434e74b0ede0f81ba
Ryan Mullin <ryan.mullin12@gmail.com>
2023-08-18 14:41:28 -0700

    Added neovim to required packages

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -5,10 +5,11 @@
 # The following packages will be installed:
 #  - ranger: A terminal file browser used as the default file explorer
 #    for the system
-#
 #  - ffmpegthumbnailer: Used by ranger to display thumbnail previews of
 #    videos
+#  - neovim: Default text editor, use instead of vim
 #
 yay -S --needed \
 	ranger \
-	ffmpegthumbnailer
+	ffmpegthumbnailer \
+  neovim
