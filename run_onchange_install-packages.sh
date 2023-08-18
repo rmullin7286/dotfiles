8609833673eecac39590c5cfaaeb191edf28b8c1
Ryan Mullin <ryan.mullin12@gmail.com>
2023-08-18 10:30:52 -0700

    Updated installer script

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
 
 # Installs necessary packages shared between systems
-sudo pacman -S --needed ripgrep i3blocks ranger ffmpegthumbnailer
+#
+# The following packages will be installed:
+#  - ranger: A terminal file browser used as the default file explorer
+#    for the system
+#
+#  - ffmpegthumbnailer: Used by ranger to display thumbnail previews of
+#    videos
+#
+yay -S --needed \
+	ranger \
+	ffmpegthumbnailer
