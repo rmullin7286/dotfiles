82bc72eddfce239bd913ecb8e15ed8b678bcbace
Ryan Mullin <ryan.mullin12@gmail.com>
2023-09-19 23:03:45 -0700

    made nvim-tree autoclose when file selected

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -68,7 +68,13 @@ require('lazy').setup({
     -- filetree
     "nvim-tree/nvim-tree.lua",
     config = function()
-      require('nvim-tree').setup()
+      require('nvim-tree').setup({
+        actions = {
+          open_file = {
+            quit_on_open = true
+          }
+        }
+      })
     end
   },
   {
