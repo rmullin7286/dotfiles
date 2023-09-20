d836997ecc9224f6f904f8e910e8610d4f5db8d0
Ryan Mullin <ryan.mullin12@gmail.com>
2023-09-20 08:33:18 -0700

    Added autoclose for nvim-tree

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -32,14 +32,14 @@ require('lazy').setup({
     branch = 'v2.x',
     dependencies = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     },
     config = function()
       -- setup required language servers
@@ -50,7 +50,7 @@ require('lazy').setup({
 
       local lsp = require('lsp-zero').preset({})
       lsp.on_attach(function(_, bufnr)
-        lsp.default_keymaps({buffer=bufnr})
+        lsp.default_keymaps({ buffer = bufnr })
       end)
 
       -- setup lua language for neovim
@@ -77,6 +77,44 @@ require('lazy').setup({
             quit_on_open = true
           }
         }
+      })
+      -- autoclose when last window
+      local function tab_win_closed(winnr)
+        return function()
+          local api = require "nvim-tree.api"
+          local tabnr = vim.api.nvim_win_get_tabpage(winnr)
+          local bufnr = vim.api.nvim_win_get_buf(winnr)
+          local buf_info = vim.fn.getbufinfo(bufnr)[1]
+          local tab_wins = vim.tbl_filter(function(w) return w ~= winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
+          local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
+          if buf_info.name:match(".*NvimTree_%d*$") then -- close buffer was nvim tree
+            -- Close all nvim tree on :q
+            if not vim.tbl_isempty(tab_bufs) then      -- and was not the last window (not closed automatically by code below)
+              api.tree.close()
+            end
+          else                                                  -- else closed buffer was normal buffer
+            if #tab_bufs == 1 then                              -- if there is only 1 buffer left in the tab
+              local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
+              if last_buf_info.name:match(".*NvimTree_%d*$") then -- and that buffer is nvim tree
+                vim.schedule(function()
+                  if #vim.api.nvim_list_wins() == 1 then        -- if its the last buffer in vim
+                    vim.cmd "quit"                              -- then close all of vim
+                  else                                          -- else there are more tabs open
+                    vim.api.nvim_win_close(tab_wins[1], true)   -- then close only the tab
+                  end
+                end)
+              end
+            end
+          end
+        end
+      end
+
+      vim.api.nvim_create_autocmd("WinClosed", {
+        callback = function()
+          local winnr = tonumber(vim.fn.expand("<amatch>"))
+          vim.schedule_wrap(tab_win_closed(winnr))
+        end,
+        nested = true
       })
     end
   },
@@ -114,36 +152,10 @@ local shell_group = vim.api.nvim_create_augroup('Shell', { clear = true })
 
 -- sets the filetype for bash files that don't have the .sh extension
 vim.api.nvim_create_autocmd(
-  {'BufRead', 'BufNewFile'},
-  { 
-    pattern={'.bashrc', '.profile', '.bash_profile'}, 
-    group=shell_group,
-    command='set filetype sh'
-  }
-)
-
--- Automatically adds a shebang to the top of a shell file on creation
--- Also automatically makes the file executable on buffer write
-vim.api.nvim_create_autocmd(
-  'BufNewFile',
+  { 'BufRead', 'BufNewFile' },
   {
+    pattern = { '.bashrc', '.profile', '.bash_profile' },
     group = shell_group,
-    pattern = "*",
-    callback = function (opts)
-      local ft = vim.filetype.match({ buf = opts.buf })
-      if ft == 'sh' then
-        vim.api.nvim_put({'#!/usr/bin/env sh', ''}, "", false, true)
-        vim.api.nvim_create_autocmd(
-          'BufWritePost',
-          {
-            buffer = opts.buf,
-            callback = function(opts) 
-              vim.fn.system({'chmod', '+x', opts.file})
-            end
-          }
-        )
-      end
-    end
+    command = 'set filetype sh'
   }
 )
-
